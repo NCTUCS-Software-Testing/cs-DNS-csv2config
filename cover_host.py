bc9cf5bf9c70bc1a4bca83396e2543fdcf9b0992
Chia-Chun Wei <weicc@cshome.cs.nctu.edu.tw>
2018-06-04 02:23:11 +0800

    for vm host

--- cover_host.py
+++ cover_host.py
@@ -6,9 +6,12 @@ import csv
 import time
 
 
-OUTPUT_HOST = 'db.private_host'
-SOURCE_DIR = 'host'
-OUTPUT_DIR = 'working'
+OUTPUT_HOST = os.path.join(
+    os.sep, 'etc', 'named', 'vm.cc.cs', 'db.private_host')
+SOURCE_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'host')
+OUTPUT_DIR = os.path.join(
+    os.sep, 'etc', 'named', 'vm.cc.cs')
 # DMZ 10.1.0.0/24
 HOST_DMZ_10_1_0 = '0.host.csv'
 OUTPUT_0_REV = '0.rev'
@@ -26,18 +29,20 @@ HOST_WWW_10_1_4 = '4.host.csv'
 OUTPUT_4_REV = '4.rev'
 # Storage 10.1.5.0/24
 HOST_STORAGE_10_1_5 = '5.host.csv'
-OUTPUT_4_REV = '5.rev'
+OUTPUT_5_REV = '5.rev'
 # VM 10.1.6.0/24
 # == VM have two special DNS ==
+HOST_VM_10_1_6 = '6.host.csv'
+OUTPUT_6_REV = '6.rev'
 # NET 10.1.7.0/24
 HOST_NET_10_1_7 = '7.host.csv'
-OUTPUT_4_REV = '7.rev'
+OUTPUT_7_REV = '7.rev'
 # PC 10.1.8.0/24
 HOST_PC_10_1_8 = '8.host.csv'
-OUTPUT_4_REV = '8.rev'
+OUTPUT_8_REV = '8.rev'
 # Mail 10.1.9.0/24
 HOST_MAIL_10_1_9 = '9.host.csv'
-OUTPUT_4_REV = '9.rev'
+OUTPUT_9_REV = '9.rev'
 
 
 def check_file_exist(filename, serial):
@@ -83,7 +88,7 @@ def open_csv_file_and_write_rev(filename, write_file):
 
 
 def write_host_config_file(data):
-    if data[3] is not '':
+    if len(data) > 3 and data[3] is not '':
         name = data[0]
         where = data[1]
         host_a = data[2].ljust(24)
@@ -110,11 +115,12 @@ def write_rev_config_file(data):
 
 
 def cover_csv_to_config():
+    print("=========================================================")
     output_host = check_host_exist(OUTPUT_HOST)
     write_file = open(output_host, mode="a+", encoding="utf-8")
-    # cover DMZ (10.1.0.0/16)
-    print("cover HOST DMZ (10.1.0.0/16)")
-    host_csv = os.path.join(SOURCE_DIR, HOST_DMZ_10_1_0)
+    # cover DMZ (10.1.6.0/24)
+    print("  cover HOST DMZ (10.1.6.0/24)")
+    host_csv = os.path.join(SOURCE_DIR, HOST_VM_10_1_6)
     if os.path.isfile(host_csv):
         open_csv_file_and_write_host(host_csv, write_file)
     # cover end
@@ -122,20 +128,22 @@ def cover_csv_to_config():
 
 
 def cover_csv_to_rev():
-    # cover DMZ (10.1.0.0/16)
-    print("cover REV DMZ (10.1.0.0/16)")
-    output_host = check_host_exist(OUTPUT_0_REV)
+    # cover VM (10.1.6.0/24)
+    print("=========================================================")
+    print("  cover REV VM (10.1.6.0/24)")
+    output_host = check_host_exist(OUTPUT_6_REV)
     write_file = open(output_host, mode="a+", encoding="utf-8")
-    host_csv = os.path.join(SOURCE_DIR, HOST_DMZ_10_1_0)
+    host_csv = os.path.join(SOURCE_DIR, HOST_VM_10_1_6)
     if os.path.isfile(host_csv):
         open_csv_file_and_write_rev(host_csv, write_file)
     write_file.close()
-    # cover DMZ (10.1.0.0/16)
+    # cover VM (10.1.6.0/24)
 
 
 def check_init_exist():
     filename = os.path.join(SOURCE_DIR, HOST_DMZ_10_1_0)
     if not os.path.isfile(filename):
+        print(filename)
         return False
     filename = os.path.join(SOURCE_DIR, HOST_CORE_10_1_1)
     if not os.path.isfile(filename):
@@ -171,7 +179,7 @@ def main():
         cover_csv_to_config()
         cover_csv_to_rev()
     else:
-        print("Check init file exist")
+        print("  Check init file exist 'FALSE', please check!")
     print("Cover CSV to DNS config complete")
 
 
