e03a3364e37df3660188693f6926ad18d1e706bd
Zih-Jing Lin <zjlin@cs.nctu.edu.tw>
2018-06-07 13:59:58 +0800

    init test_cover_host.py

---
+++ test_cover_host.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import unittest
+from cover_host.py import *
+
+
+class Test_cover_host(unittest.TestCase):
+    """ Test cover_host.py """
+
+
+if __name__ == "__main__":
+    unittest.main()
